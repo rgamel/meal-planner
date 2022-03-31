a0d900cd8fa3b7c757d37fe536fdec172a8491af
Ryan Gamel <ryan.gamel@gmail.com>
2022-03-31 01:35:34 -0500

    once more for luck

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -2,9 +2,18 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore/lite'; // TODO
 import { getFunctions } from 'firebase/functions';
-import config from '../firebase-config.json';
 
-export const fire = initializeApp(config.result.sdkConfig);
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: 'mealplanner-ddb4f.firebaseapp.com',
+    projectId: 'mealplanner-ddb4f',
+    storageBucket: 'mealplanner-ddb4f.appspot.com',
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+export const fire = initializeApp(firebaseConfig);
 export const auth = getAuth(fire);
 export const db = getFirestore(fire);
 export const functions = getFunctions();
