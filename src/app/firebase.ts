203dccb4f75959b2bffd11ab0f5fb1a7d1882e48
Ryan Gamel <gcsxl@monsanto.com>
2022-03-31 23:36:10 -0500

    throwing in the towel

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore/lite'; // TODO
 import { getFunctions } from 'firebase/functions';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    apiKey: 'AIzaSyB19kiBtlI3chGsXiIHIqpIhgb9B_4r5OI',
     authDomain: 'mealplanner-ddb4f.firebaseapp.com',
     projectId: 'mealplanner-ddb4f',
     storageBucket: 'mealplanner-ddb4f.appspot.com',
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+    messagingSenderId: '1001789350756',
+    appId: '1:1001789350756:web:675cf99885b52701781071',
+    measurementId: 'G-S45HKCZD6P',
 };
 
 export const fire = initializeApp(firebaseConfig);
