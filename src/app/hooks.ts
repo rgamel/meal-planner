540b5af344aea27c0b2bee71df51029e20a6f179
Ryan Gamel <ryan.gamel@gmail.com>
2024-03-09 11:33:29 -0600

    test commit

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -5,8 +5,6 @@ import { Dispatch, SetStateAction, useContext } from 'react';
 import { db } from './firebase';
 import { RecipesContext } from './recipeContext';
 
-console.log('nonsense commit');
-
 export const useFirebase = () => {
     const formatCollectionAsList = (snapshot: QuerySnapshot) =>
         snapshot.docs.reduce(
