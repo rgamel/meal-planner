c002a47261ffb13b37300b48e0c259bb4a3cbdd3
Ryan Gamel <ryan.gamel@gmail.com>
2024-04-10 14:55:55 -0500

    use zustand for user storage

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import { CssBaseline } from '@mui/material';
-import UserContextProvider from 'app/userContext';
 import { Home } from 'features/home/Home';
 import { ConfirmProvider } from 'material-ui-confirm';
 import React from 'react';
@@ -15,24 +14,22 @@ import './styles.css';
 
 ReactDOM.render(
     <React.StrictMode>
-        <UserContextProvider>
-            <RecipesContextProvider>
-                <ConfirmProvider>
-                    <BrowserRouter>
-                        <Routes>
-                            <Route path="/" element={<App />}>
-                                <Route path="" element={<Home />} />
-                                <Route path="recipes" element={<Recipes />} />
-                                <Route path="groceries" element={<Groceries />} />
-                                <Route path="plans" element={<Plans />} />
-                                <Route path="plans/:id" element={<Plan />} />
-                            </Route>
-                        </Routes>
-                    </BrowserRouter>
-                </ConfirmProvider>
-            </RecipesContextProvider>
-            <CssBaseline />
-        </UserContextProvider>
+        <RecipesContextProvider>
+            <ConfirmProvider>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<App />}>
+                            <Route path="" element={<Home />} />
+                            <Route path="recipes" element={<Recipes />} />
+                            <Route path="groceries" element={<Groceries />} />
+                            <Route path="plans" element={<Plans />} />
+                            <Route path="plans/:id" element={<Plan />} />
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+            </ConfirmProvider>
+        </RecipesContextProvider>
+        <CssBaseline />
     </React.StrictMode>,
     document.getElementById('root'),
 );
