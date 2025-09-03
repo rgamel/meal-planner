c1f28dca01dcfdecb6f9028ca21424f4d8ec0d35
Ryan Gamel <ryan.gamel@gmail.com>
2025-09-02 23:33:39 -0500

    wip breaking up plan component

--- src/features/plans/Plan.tsx
+++ src/features/plans/Plan.tsx
@@ -11,104 +11,23 @@ import { useCallback, useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { EmptyPlanMessage } from './EmptyPlanMessage';
 import { PlanItem } from './PlanItem';
+import { Plan as IPlan } from 'types';
 
 export default function Plan(): JSX.Element {
     const planId = useParams().id as string;
-    const { plans, setSelectedPlanId, deletePlan, updatePlan } = usePlans();
-    const nav = useNavigate();
-    const confirm = useConfirm();
+    const { plans, setSelectedPlanId } = usePlans();
 
     const plan = plans[planId];
-
-    const [editingName, setEditingName] = useState(false);
-    const [planName, setPlanName] = useState(titleCase(plan?.name));
+    const hasRecipes = !isEmpty(plan?.recipes ?? []);
 
     useEffect(() => {
         setSelectedPlanId(planId);
     }, [planId, setSelectedPlanId]);
 
-    const handleDelete = () => {
-        confirm({
-            title: `Delete ${planName}?`,
-            description: 'This cannot be undone',
-            confirmationButtonProps: { color: 'error', variant: 'contained' },
-        })
-            .then(() => {
-                deletePlan(planId);
-                nav('/plans');
-            })
-            .catch(noop);
-    };
-
-    const onInputChange = (e: { target: { value: string } }) => {
-        const inputValue = e.target.value ?? '';
-        if (!inputValue.trim().length) return;
-        setPlanName(e.target.value);
-    };
-
-    const navToRecipes = useCallback(() => {
-        setSelectedPlanId(planId);
-        nav('/recipes');
-    }, [planId, setSelectedPlanId, nav]);
-
-    const navToGroceries = useCallback(() => {
-        setSelectedPlanId(planId);
-        nav('/groceries');
-    }, [planId, setSelectedPlanId, nav]);
-
-    const hasRecipes = !isEmpty(plan?.recipes ?? []);
-
     return (
         <>
-            <div className="align-center flex flex-row justify-around">
-                <IconButton
-                    className="m-2 p-2"
-                    onClick={() => {
-                        nav('/plans');
-                    }}
-                >
-                    <div className="opacity-50">
-                        <ArrowBack />
-                    </div>
-                </IconButton>
-                {editingName ? (
-                    <>
-                        <input
-                            className="block w-full rounded-lg py-2 pl-6 pr-20 text-lg ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-blue-700"
-                            value={planName}
-                            onChange={onInputChange}
-                        />
-                        <IconButton
-                            className="m-2 p-2"
-                            onClick={() => {
-                                updatePlan({ ...plan, name: planName });
-                                setEditingName(false);
-                            }}
-                        >
-                            <div className="opacity-50">
-                                <Save />
-                            </div>
-                        </IconButton>
-                    </>
-                ) : (
-                    <>
-                        <PageTitle>
-                            <span className="align-middle">{planName}</span>
-                        </PageTitle>
-                        <IconButton
-                            className="m-2 p-2"
-                            type="button"
-                            onClick={() => {
-                                setEditingName(true);
-                            }}
-                        >
-                            <div className="opacity-50">
-                                <Edit />
-                            </div>
-                        </IconButton>
-                    </>
-                )}
-            </div>
+            <PlanHeader />
+
             {hasRecipes ? (
                 <div className="m-4">
                     <ul className="w-full divide-y divide-gray-200 rounded-xl border-2 border-gray-200 bg-white">
@@ -125,31 +44,169 @@ export default function Plan(): JSX.Element {
                 <EmptyPlanMessage />
             )}
 
-            <div className="flex flex-col">
-                <div className="m-4 inline-flex">
-                    <Button
-                        type="button"
-                        variant={!hasRecipes ? 'contained' : 'outlined'}
-                        className="w-full rounded-none rounded-l-md"
-                        onClick={navToRecipes}
-                    >
-                        Recipes
-                    </Button>
-                    <Button
-                        type="button"
-                        variant={hasRecipes ? 'contained' : 'outlined'}
-                        className="w-full rounded-none rounded-r-md"
-                        onClick={navToGroceries}
-                    >
-                        Groceries
-                    </Button>
-                </div>
-                <div className="flex justify-center">
-                    <DeleteButton type="button" onClick={handleDelete}>
-                        Delete Plan
-                    </DeleteButton>
-                </div>
-            </div>
-        </>
+            <PlanActions planId={planId} plan={plan} />        </>
     );
+}
+
+function PlanHeader(): JSX.Element {
+    const { plans, setSelectedPlanId } = usePlans();
+    const planId = useParams().id as string;
+    const plan = plans[planId];
+    const [editingName, setEditingName] = useState(false);
+    const [planName, setPlanName] = useState(titleCase(plan?.name));
+    const nav = useNavigate();
+
+    useEffect(() => {
+        setSelectedPlanId(planId);
+    }, [planId, setSelectedPlanId]);
+
+    function navToPlans() {
+        nav('/plans')
+    }
+
+    function onInputChange(e: { target: { value: string } }) {
+        const inputValue = e.target.value ?? ''
+        if (inputValue.trim() === '') {
+            return;
+        }
+        setPlanName(inputValue)
+    }
+
+    return (
+        <div className="align-center flex flex-row justify-around">
+            <IconButton
+                className="m-2 p-2"
+                onClick={navToPlans}
+            >
+                <div className="opacity-50">
+                    <ArrowBack />
+                </div>
+            </IconButton>
+
+            <PlanName editingName={editingName} planName={planName} onInputChange={onInputChange} />
+            <ActionButton editing={editingName} setEditingName={setEditingName} plan={plan} planName={planName} />
+        </div>
+    )
+}
+
+type PlanNameProps = {
+    editingName: boolean
+    planName: string
+    onInputChange(e: { target: { value: string } }): void
+}
+
+function PlanName({ editingName, planName, onInputChange }: PlanNameProps) {
+    if (!editingName) {
+        return (
+            <PageTitle>
+                <span className="align-middle">{planName}</span>
+            </PageTitle>
+
+        )
+    }
+    return (
+        <input
+            className="block w-full rounded-lg py-2 pl-6 pr-20 text-lg ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-blue-700"
+            value={planName}
+            onChange={onInputChange}
+        />
+
+    )
+}
+
+function ActionButton({ editing, plan, planName, setEditingName }: {
+    plan: IPlan
+    planName: string
+    editing: boolean
+    setEditingName(input: boolean): void
+}) {
+    const { updatePlan } = usePlans()
+
+    function startEditingName() {
+        setEditingName(true);
+    }
+
+    function stopEditingName() {
+        updatePlan({ ...plan, name: planName });
+        setEditingName(false);
+    }
+
+    const clickHandler = editing ? stopEditingName : startEditingName
+    const Icon = editing ? Save : Edit
+
+    return (
+        <IconButton
+            className="m-2 p-2"
+            onClick={clickHandler}
+        >
+            <div className="opacity-50">
+                <Icon />
+            </div>
+        </IconButton>
+    )
+}
+
+type PlanActionsProps = {
+    planId: string
+    plan: IPlan
+}
+function PlanActions({ planId, plan }: PlanActionsProps) {
+    const { setSelectedPlanId, deletePlan } = usePlans();
+    const nav = useNavigate()
+    const confirm = useConfirm()
+
+
+    const navToRecipes = useCallback(() => {
+        setSelectedPlanId(planId);
+        nav('/recipes');
+    }, [planId, setSelectedPlanId, nav]);
+
+    const navToGroceries = useCallback(() => {
+        setSelectedPlanId(planId);
+        nav('/groceries');
+    }, [planId, setSelectedPlanId, nav]);
+
+    const hasRecipes = !isEmpty(plan?.recipes ?? []);
+
+    const handleDelete = () => {
+        confirm({
+            title: `Delete ${plan.name}?`,
+            description: 'This cannot be undone',
+            confirmationButtonProps: { color: 'error', variant: 'contained' },
+        })
+            .then(() => {
+                deletePlan(planId);
+                nav('/plans');
+            })
+            .catch(noop);
+    };
+
+    return (
+        <div className="flex flex-col">
+            <div className="m-4 inline-flex">
+                <Button
+                    type="button"
+                    variant={!hasRecipes ? 'contained' : 'outlined'}
+                    className="w-full rounded-none rounded-l-md"
+                    onClick={navToRecipes}
+                >
+                    Recipes
+                </Button>
+                <Button
+                    type="button"
+                    variant={hasRecipes ? 'contained' : 'outlined'}
+                    className="w-full rounded-none rounded-r-md"
+                    onClick={navToGroceries}
+                >
+                    Groceries
+                </Button>
+            </div>
+            <div className="flex justify-center">
+                <DeleteButton type="button" onClick={handleDelete}>
+                    Delete Plan
+                </DeleteButton>
+            </div>
+        </div>
+
+    )
 }
