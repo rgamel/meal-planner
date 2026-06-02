b381d377e70eccb9b148064c2ee52c87d1edf0a9
Ryan Gamel <ryan.gamel@gmail.com>
2026-06-01 21:13:39 -0500

    enable folding recipe sections

--- src/features/recipes/RecipeCategorySection.tsx
+++ src/features/recipes/RecipeCategorySection.tsx
@@ -1,5 +1,7 @@
 import { useCategories } from 'app/hooks'
+import clsx from 'clsx'
 import { titleCase } from 'helpers'
+import { useState } from 'react'
 import { Recipe } from 'types'
 
 import RecipeListItem from './RecipeListItem'
@@ -13,13 +15,38 @@ export function RecipeCategorySection({ recipes, editRecipe, categoryId }: Recip
   const { categories } = useCategories()
   const label = titleCase(categories[categoryId]?.name)
   return (
-    <div className="px-6 py-4">
-      <h2 className="flex text-xl font-semibold text-gray-900">{label}</h2>
-      <ul>
-        {recipes.map((r) => (
-          <RecipeListItem key={r.id} recipe={r} editRecipe={editRecipe} />
-        ))}
-      </ul>
+    <div className="px-6">
+      <Accordion
+        heading={<h2 className="flex py-4 text-xl font-semibold text-gray-900">{label}</h2>}
+        content={
+          <ul>
+            {recipes.map((r) => (
+              <RecipeListItem key={r.id} recipe={r} editRecipe={editRecipe} />
+            ))}
+          </ul>
+        }
+      />
+    </div>
+  )
+}
+
+type AccordionProps = {
+  heading: React.ReactNode
+  content: React.ReactNode
+}
+function Accordion({ heading, content }: AccordionProps) {
+  const [open, setOpen] = useState(true)
+  function toggleOpen() {
+    setOpen((prev) => !prev)
+  }
+  const icon = open ? 'keyboard_arrow_up' : 'keyboard_arrow_down'
+  return (
+    <div>
+      <button className="flew-row flex w-full items-center justify-between active:bg-slate-200" onClick={toggleOpen}>
+        {heading}
+        <span className="material-symbols-outlined">{icon}</span>
+      </button>
+      <div className={clsx('overflow-hidden', open ? 'max-h-none' : 'max-h-0 ')}>{content}</div>
     </div>
   )
 }
