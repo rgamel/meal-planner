b9b72726b921228722b6599672df23f8085b2caf
Ryan Gamel <ryan.gamel@gmail.com>
2026-06-01 23:24:40 -0500

    migrate vite to v8

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   // depending on your application, base can also be "/"
   base: '',
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [react()],
+  resolve: { tsconfigPaths: true },
   server: {
     // this ensures that the browser opens upon server start
     open: true,
