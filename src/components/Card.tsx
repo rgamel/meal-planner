ec28adcac06626f17ee9ee903394f70e841e8396
Ryan Gamel <ryan.gamel@gmail.com>
2026-06-01 21:48:59 -0500

    run migration tool

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,7 +5,7 @@ type Props = React.HTMLAttributes<any>
 export function Card({ className, ...rest }: Props) {
   return (
     <div
-      className={clsx('m-6 flex flex-col rounded-xl border-2 border-gray-200 bg-white shadow-sm', className)}
+      className={clsx('m-6 flex flex-col rounded-xl border-2 border-gray-200 bg-white shadow-xs', className)}
       {...rest}
     />
   )
--- src/features/recipes/RecipeListItem.tsx
+++ src/features/recipes/RecipeListItem.tsx
@@ -32,7 +32,7 @@ export default function RecipeListItem({ recipe, editRecipe }: RecipeListItemPro
         <div className="text-md">{label}</div>
       </div>
       <IconButton className="absolute right-0 pr-0" onClick={onClickEdit}>
-        <div className="-mr-0 opacity-50">
+        <div className="mr-0 opacity-50">
           <Edit />
         </div>
       </IconButton>
